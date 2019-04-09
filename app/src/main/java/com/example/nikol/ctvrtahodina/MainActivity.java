8a15e84a34c2f7cd3da15276b6ba1452befce0dd
NikolPl <n.plavjanikova@gmail.com>
2019-04-09 21:13:56 +0200

    Initial commit

---
+++ app/src/main/java/com/example/nikol/ctvrtahodina/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.nikol.ctvrtahodina;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.image);
+        Glide.with(this).load("https://img.csfd.cz/files/images/film/posters/162/505/162505167_735db9.jpg?h180").into(imageView);
+
+
+    }
+}
